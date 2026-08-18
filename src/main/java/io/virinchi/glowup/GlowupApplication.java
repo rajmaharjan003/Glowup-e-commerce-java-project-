6883b7fe968d493273ebaaece3cadec90f52c73b
rajmaharjan003 <rajmaharjan738@gmail.com>
2026-08-18 09:24:23 +0545

    java project updated

--- src/main/java/io/virinchi/glowup/GlowupApplication.java
+++ src/main/java/io/virinchi/glowup/GlowupApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GlowupApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GlowupApplication.class, args);
-    }
 
+        SpringApplication.run(
+                GlowupApplication.class,
+                args
+        );
+    }
 }
